96b0a77175c0b48d1710e3c916b3acecc60cab9c
Dichik <omeluan.dima@gmail.com>
2023-03-11 06:48:24 +0200

    assignment 1: add starter code

---
+++ assignment-1/src/main/java/org/assignment_1/Bounce.java
@@ -0,0 +1,17 @@
+package org.assignment_1;
+
+import org.assignment_1.entity.BounceFrame;
+
+import javax.swing.*;
+
+public class Bounce {
+    public static void main(String[] args) {
+        BounceFrame frame = new BounceFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+        System.out.println("Thread name = " +
+                Thread.currentThread().getName());
+
+    }
+}
