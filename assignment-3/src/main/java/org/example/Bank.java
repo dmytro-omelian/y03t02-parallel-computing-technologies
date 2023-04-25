6b3e56e9b17c14dc7c35399553086d954325a2e6
Dichik <omeluan.dima@gmail.com>
2023-04-25 14:03:10 +0300

    tech: code cleanup

--- assignment-3/src/main/java/org/example/Bank.java
+++ assignment-3/src/main/java/org/example/Bank.java
@@ -4,13 +4,15 @@ class Bank {
     public static final int NTEST = 10000;
     private final int[] accounts;
     private long ntransacts = 0;
-    public Bank(int n, int initialBalance){
+
+    public Bank(int n, int initialBalance) {
         accounts = new int[n];
         int i;
         for (i = 0; i < accounts.length; i++)
             accounts[i] = initialBalance;
         ntransacts = 0;
     }
+
     public void transfer(int from, int to, int amount) {
         accounts[from] -= amount;
         accounts[to] += amount;
@@ -18,13 +20,15 @@ class Bank {
         if (ntransacts % NTEST == 0)
             test();
     }
-    public void test(){
+
+    public void test() {
         int sum = 0;
         for (int i = 0; i < accounts.length; i++)
-            sum += accounts[i] ;
+            sum += accounts[i];
         System.out.println("Transactions:" + ntransacts + " Sum: " + sum);
     }
-    public int size(){
+
+    public int size() {
         return accounts.length;
     }
 }
