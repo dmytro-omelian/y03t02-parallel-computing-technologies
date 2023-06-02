3c6db0e29a7bf45277b815191e8f8056d5e2daa0
Dichik <omeluan.dima@gmail.com>
2023-06-02 12:13:15 +0300

    tech: minor updates

--- assignment-8/client/client.js
+++ assignment-8/client/client.js
@@ -5,7 +5,7 @@ function createMatrix() {
     const rows = document.getElementById('rows').value;
     const columns = document.getElementById('columns').value;
 
-    console.time('createMatrix'); // Start measuring time
+    console.time('createMatrix');
 
     fetch(`${baseUrl}/api/matrix/create?filename=${filename}&rows=${rows}&columns=${columns}`, {
         method: 'POST',
@@ -18,7 +18,7 @@ function createMatrix() {
             }
         })
         .then(result => {
-            console.timeEnd('createMatrix'); // End measuring time
+            console.timeEnd('createMatrix');
             const resultDiv = document.getElementById('result');
             resultDiv.innerHTML += `<p>${result}</p>`;
         })
@@ -29,12 +29,12 @@ function multiplyOnServer() {
     const matrixAFilename = document.getElementById('matrixAFilename').value;
     const matrixBFilename = document.getElementById('matrixBFilename').value;
 
-    console.time('multiplyOnServer'); // Start measuring time
+    console.time('multiplyOnServer'); 
 
     fetch(`${baseUrl}/api/matrix/multiply-server?matrixAFilename=${matrixAFilename}&matrixBFilename=${matrixBFilename}`)
         .then(response => response.json())
         .then(result => {
-            console.timeEnd('multiplyOnServer'); // End measuring time
+            console.timeEnd('multiplyOnServer'); 
             const resultDiv = document.getElementById('result');
             resultDiv.innerHTML += `<p>Результат множення: ${JSON.stringify(result)}</p>`;
         })
@@ -45,7 +45,7 @@ function multiplyFromClient() {
     const matrixAFile = document.getElementById('matrixA').files[0];
     const matrixBFile = document.getElementById('matrixB').files[0];
 
-    console.time('multiplyFromClient'); // Start measuring time
+    console.time('multiplyFromClient');
 
     const formData = new FormData();
     formData.append('matrixA', matrixAFile);
@@ -60,7 +60,7 @@ function multiplyFromClient() {
     })
         .then(response => response.json())
         .then(result => {
-            console.timeEnd('multiplyFromClient'); // End measuring time
+            console.timeEnd('multiplyFromClient'); 
             const resultDiv = document.getElementById('result');
             resultDiv.innerHTML += `<p>Результат множення: ${JSON.stringify(result)}</p>`;
         })
