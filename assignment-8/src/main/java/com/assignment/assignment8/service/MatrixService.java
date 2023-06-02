38d55b0905d1f5572a90cd9f1306589592bb4055
Dichik <omeluan.dima@gmail.com>
2023-06-02 10:15:49 +0300

    8. created MatrixService

---
+++ assignment-8/src/main/java/com/assignment/assignment8/service/MatrixService.java
@@ -0,0 +1,26 @@
+package com.assignment.assignment8.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class MatrixService {
+    private static final Random random = new Random();
+
+
+    public MatrixService() {
+
+    }
+
+    public double[][] generate(int rows, int columns) {
+        double[][] result = new double[rows][columns];
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < columns; ++j) {
+                result[i][j] = 100 * random.nextDouble();
+            }
+        }
+        return result;
+    }
+
+}
